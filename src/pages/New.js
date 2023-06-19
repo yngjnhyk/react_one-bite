c9ead945b1a89a8a7c670702022476d43a494830
yngjnhyk <dskzpbmk@naver.com>
2023-06-19 18:33:15 +0900

    first commit

--- src/pages/New.js
+++ src/pages/New.js
@@ -1,5 +1,30 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../component/Button';
+import Header from '../component/Header';
+import Editor from '../component/Editor';
+import { useContext } from 'react';
+import { DiaryDispatchContext } from '../App';
+
 const New = () => {
-  return <div>New 페이지입니다</div>;
+  const onSubmit = (data) => {
+    const { date, content, emotionId } = data;
+    onCreate(date, content, emotionId);
+    navigate('/', { replace: true });
+  };
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+  return (
+    <div>
+      <Header
+        title={'새 일기 쓰기'}
+        leftChild={<Button text={'뒤로 가기'} onClick={goBack} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
